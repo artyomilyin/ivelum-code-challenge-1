2c71704d09fa997420316897f54d8054864c1bca
Artyom ILYIN <artyom_ilyin@mail.ru>
2019-04-12 13:06:01 +0300

    Parser should exclude <code> tag from regex search. All the tags that should be excluded are in separate list `exclude_tags`.

--- server.py
+++ server.py
@@ -37,7 +37,9 @@ class IvelumRequestHandler(SimpleHTTPRequestHandler):
 
             # find every element which 'text' parameter matching the re_pattern and not being a script
             # then add ™ char to every 6-letter word
-            found_lines = [line for line in soup.find_all(text=re.compile(re_pattern)) if line.parent.name != 'script']
+            exclude_tags = ['script', 'code']
+            found_lines = [line for line in soup.find_all(text=re.compile(re_pattern))
+                           if line.parent.name not in exclude_tags]
             for line in found_lines:
                 new_line = re.sub(re_pattern, r'\1™', line)
                 line.replaceWith(new_line)
